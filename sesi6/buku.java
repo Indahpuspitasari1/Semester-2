fd3a14564597bf2ba3c4069825a990ac82d47af3
Indahpuspitasari1 <114733787+Indahpuspitasari1@users.noreply.github.com>
2023-03-26 22:22:50 +0700

    Add files via upload

---
+++ sesi6/buku.java
@@ -0,0 +1,17 @@
+public class buku {
+    public String judul;
+    public String pengarang;
+    public String penerbit;
+    public int tahun ;
+    public int kategori;
+    public String keterangan;
+
+    public buku(String judul, String pengarang, String penerbit, int tahun, int kategori, String keterangan){
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.penerbit = penerbit;
+        this.tahun = tahun;
+        this.kategori = kategori;
+        this.keterangan = keterangan;
+    }
+}
